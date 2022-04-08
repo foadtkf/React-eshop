2ddd681ef42b1a6e4b24d6826c1e2dd72ac8ce10
foadtkf <fakhrulif@gmail.com>
2022-04-08 15:20:49 +0600

    react router used

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
 import Header from './Components/Header/Header';
-// import Shop from './Components/Shop/Shop';
+import Shop from './Components/Shop/Shop';
 
-import Test from './Components/Shop/Test';
+// import Test from './Components/Shop/Test';
 
 function App() {
   return (
     <div >
       <Header></Header>
       {/* <Shop></Shop> */}
-      <Test></Test>
+      <Routes>
+        <Route path='/shop' element={<Shop></Shop>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../images/Logo.svg'
 import  './Header.css';
 const Header = () => {
@@ -6,7 +7,11 @@ const Header = () => {
        <nav className='Header'>
        <img src={logo} alt=""></img>
         <div className='header-buttons'>
-            <a href="..">Home</a><a href="..">Order</a><a href="..">FAQ</a><a href="..">About</a>
+            <a href="/shop">Home</a>
+        <Link to='/shop'>Shop</Link>
+        <a href="..">Order</a>
+        <a href="..">FAQ</a>
+        <Link to='/about'>About</Link>
             </div>
 
        </nav>
